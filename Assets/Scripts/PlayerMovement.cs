575d0a187777fd9ebed5ce43b8faa95d05417f11
512233 <512233@chrome.escg.ac.uk>
2021-11-19 13:30:33 +0000

    Added jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class PlayerMovement : MonoBehaviour
 {
 
+    Animator animator; 
+
     bool alive = true;
+
+    public bool isGrounded = false;
 
     public float speed = 5;
     [SerializeField] Rigidbody rb;
@@ -14,6 +19,16 @@ public class PlayerMovement : MonoBehaviour
 
     public float speedIncreasePerPoint = 0.1f;
 
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
     private void FixedUpdate()
     {
         if (!alive) return;
@@ -21,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
+
+       
     }
 
     private void Update()
@@ -31,18 +48,49 @@ public class PlayerMovement : MonoBehaviour
         {
             Die();
         }
+
+        if (Input.GetKeyDown("space")&& isGrounded == true)
+        {
+            //Vector3 forwardJump= transform.up * speed * Time.fixedDeltaTime;
+            Debug.Log("Jump");
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            animator.SetBool("IsJumping", true); 
+        }
+    }
+
+
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Plane")
+        {
+            isGrounded = true;
+            animator.SetBool("IsJumping", false);
+        }
+    }
+
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Plane")
+        {
+            isGrounded = false;
+            animator.SetBool("IsJumping", true);
+        }
     }
 
     public void Die()
     {
         alive = false;
         // Restart the game
-        Invoke("Restart", 2);
+        Invoke("Restart", 1);
     }
 
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+  
+
 
 }
